4abc32f518adbbb3d8e150aa081cd206b19a4920
Thitiratratrat <altothiti@gmail.com>
2021-02-18 00:17:42 +0700

    edit base url

--- src/lib/dependencies.js
+++ src/lib/dependencies.js
@@ -2,7 +2,7 @@ import TodoService from '../services/TodoService';
 import axios from 'axios';
 
 const todoService = new TodoService(
-  axios.create({ baseURL: 'http://34.87.78.160:5000/' })
+  axios.create({ baseURL: 'https://thinks-itself-belts-lucky.trycloudflare.com/' })
 );
 
 export { todoService };
